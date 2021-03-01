32d0aa75b462ae123b69e41be0e55ed4829a84d1
venkata anil <anilvenkata@redhat.com>
2021-03-01 14:13:19 +0530

    add hosts

---
+++ add_hosts.sh
@@ -0,0 +1,24 @@
+
+sudo sed -i "s/root/heat-admin/g" ~/.ssh/config
+sudo cp /etc/hosts ~/
+sudo cp /etc/hosts /etc/hosts_org
+source ~/stackrc
+total_nodes=`openstack server list -c ID -f value | wc -l`
+controllers=3
+computes=$((total_nodes-controllers))
+echo computes: $computes
+for i in $(seq 0 $((computes-1))); do
+  sudo sed -i "s/compute-$i.ctlplane$/compute-$i.ctlplane cp-$i/g" /etc/hosts
+done  
+for i in $(seq 0 $((controllers-1))); do
+  sudo sed -i "s/controller-$i.ctlplane$/controller-$i.ctlplane ct-$i/g" /etc/hosts
+done  
+
+for i in $(seq 0 $((computes-1))); do
+  ssh cp-$i "sudo cp -r ~/.ssh /root/";
+done
+for i in $(seq 0 $((controllers-1))); do
+  ssh ct-$i "sudo cp -r ~/.ssh /root/";
+done
+
+sudo sed -i "s/heat-admin/root/g" ~/.ssh/config
